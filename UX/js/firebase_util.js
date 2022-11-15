b746f04e2494fa5f433f82f5b1798c9d248ff6f3
Alba Oliver <aolinav@epsg.upv.es>
2022-11-15 20:15:51 +0100

    Css

--- UX/js/firebase_util.js
+++ UX/js/firebase_util.js
@@ -88,7 +88,8 @@ export default class FirebaseUtil{
             } else {
                 // User is signed out
                 // ...
-                window.location.replace("./correo_no_confirmado.html");
+                //window.location.replace("./correo_no_confirmado.html");
+                no_confirmado.showModal();
             }
             
         })
@@ -100,6 +101,7 @@ export default class FirebaseUtil{
         });
         
     }
+
 
     // .................................................................
     // email,password:str
