dc606e76f0af990631c720024648dace3306d2e4
Carlos <cipiqui@epsg.upv.es>
2022-11-19 20:50:32 +0100

    tabla micros

---
+++ BD/TablaMicros.sql
@@ -0,0 +1,13 @@
+
+-- create table Micros
+
+DROP TABLE Micros;
+
+create table Micros(
+
+    correoUser varchar(20) NOT NULL,
+    IdMicro int NOT NULL,
+    Disponibilidad boolean NOT NULL,
+    UltimaVezActivo DATE NOT NULL
+
+);
