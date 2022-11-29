201eba14cc1b6b6da383dd8434cbf6d9ef71a171
Daniel Benavides Belmonte <ruxzfly@gmail.com>
2022-11-29 18:03:55 +0100

    Modificar BD para incluir latitud y longitud en tabla sensor

--- BD/TablaMedicion.sql
+++ BD/TablaMedicion.sql
@@ -6,7 +6,9 @@ create table Medicion (
     id INTEGER PRIMARY KEY,
     muestra char(8) NOT NULL,
     fecha DATE NOT NULL,
-    usuario char(12) NOT NULL
+    usuario char(12) NOT NULL,
+    latitud float NOT NULL,
+    longitud float NOT NULL
 );
-INSERT INTO Medicion VALUES (NULL, "1234", '2022-10-18', "Dani");
-INSERT INTO Medicion VALUES (NULL, "1235", '2022-10-19', "Dani");
+INSERT INTO Medicion VALUES (NULL, "1234", '2022-10-18', "Dani", 38.999794344442414, -0.16320239995270047);
+INSERT INTO Medicion VALUES (NULL, "1235", '2022-10-19', "Dani", 38.999469165763365, -0.16296636556388053);
