9c322649b1d42a1b46981cc21349cb7a765e8bfc
Alba Oliver <aolinav@epsg.upv.es>
2022-12-04 19:24:02 +0100

    Valores

--- UX/js/grafica.js
+++ UX/js/grafica.js
@@ -19,7 +19,7 @@ const labels = ['00h', '01h', '02h', '03h', '04h', '05h', '06h', '07h', '08h', '
  
 const dataset1 = {
     label: "Peligro",
-    data: [10, 55, 60, 120, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 4, 3, 2, 1, 90, 80, 70, 0, 0],
+    data: [0, 0, 0 ,0 , 0, 0, 40, 70, 60, 80, 100, 110, 95, 90, 90, 70, 40, 2, 1 ,2 ,1 ,2 , 0, 0],
     borderColor: 'rgba(248, 37, 37, 0.8)',
     fill: false,
     tension: 0.1
@@ -27,7 +27,7 @@ const dataset1 = {
  
 const dataset2 = {
     label: "Concentración elevada",
-    data: [5, 44, 55, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 4, 3, 2, 1, 90, 80, 70, 0, 0, 0],
+    data: [0, 0, 0, 0, 0, 0, 0, 60, 50, 70, 90, 90, 95, 80, 60, 3, 2, 1, 3, 3, 2, 1, 0],
     borderColor: 'rgb(247,126,0.8)',
     fill: false,
     tension: 0.1
@@ -35,7 +35,7 @@ const dataset2 = {
  
 const dataset3 = {
     label: "Concentración baja",
-    data: [20, 40, 55, 120, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 4, 3, 2, 90, 80, 70, 0, 0, 0],
+    data: [110, 115, 100, 120, 100, 90, 90, 70, 60, 50, 40, 30, 20, 10, 5, 4, 3, 2, 55, 100, 80, 90, 55, 60],
     borderColor: 'rgba(69, 140, 248, 0.8)',
     fill: false,
     tension: 0.1
@@ -43,7 +43,7 @@ const dataset3 = {
  
 const dataset4 = {
     label: "Concentración sin riesgo",
-    data: [18, 40, 20, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5, 4, 3, 2, 1, 0, 90, 80, 70, 0, 0],
+    data: [60, 70, 65, 80, 90, 80, 100, 80, 50, 0, 0, 0, 3, 5, 4, 3, 2, 1, 50, 90, 80, 70, 65, 70],
     borderColor: 'rgb(111,245,0.8)',
     fill: false,
     tension: 0.1
